50e7b490bbe58e685408dc9d333a91aa5597ff8c
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-11-22 17:00:03 +0500

    commit

--- lib/detailed_scree.dart
+++ lib/detailed_scree.dart
@@ -1,22 +1,27 @@
 import 'package:e_cmmerce_hey_flutter/availble_size.dart';
 import 'package:e_cmmerce_hey_flutter/product.dart';
 import 'package:flutter/material.dart';
-class DetailedScreen extends StatelessWidget {
 
+class DetailedScreen extends StatelessWidget {
   final Product product;
-  const DetailedScreen({super.key,required this.product});
+  const DetailedScreen({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Detailed Screen",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Detailed Screen",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
       body: Column(
         children: [
-          SizedBox(height: 40,),
+          SizedBox(
+            height: 40,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -27,24 +32,26 @@ class DetailedScreen extends StatelessWidget {
                   borderRadius: BorderRadius.circular(100),
                   color: Colors.red.shade100,
                 ),
-                child: Image.asset(product.image,
-                  fit: BoxFit.cover,),
+                child: Image.asset(
+                  product.image,
+                  fit: BoxFit.cover,
+                ),
               )
             ],
           ),
-          SizedBox(height: 36,),
+          SizedBox(
+            height: 36,
+          ),
           Container(
             padding: EdgeInsets.all(20),
             height: 400,
             width: double.infinity,
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(40),
-                topLeft: Radius.circular(40),
-
-              )
-            ),
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(40),
+                  topLeft: Radius.circular(40),
+                )),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -53,47 +60,68 @@ class DetailedScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(product.name.toUpperCase(),style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                    ),),
-                    Text('\$''${product.price}',style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),),
-
+                    Text(
+                      product.name.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '\$' '${product.price}',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(height: 20,),
-                Text(product.description,
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  product.description,
                   textAlign: TextAlign.justify,
                   style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),),
-                SizedBox(height: 20,),
-                Text("Availble Sizes",style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),),
-                SizedBox(height: 20,),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Availble Sizes",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Row(
-
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     AvailbleSize(size: "US 6"),
                     AvailbleSize(size: "US 7"),
                     AvailbleSize(size: "US 8"),
                     AvailbleSize(size: "US 9"),
-
                   ],
                 ),
-                SizedBox(height: 20,),
-                Text("Availble Colour",style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),),
-                SizedBox(height: 20,),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Availble Colour",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Row(
                   children: [
                     Container(
@@ -104,16 +132,20 @@ class DetailedScreen extends StatelessWidget {
                         shape: BoxShape.circle,
                       ),
                     ),
-                    SizedBox(width: 10,),
+                    SizedBox(
+                      width: 10,
+                    ),
                     Container(
-                      height: 22,
+                      height: 23,
                       width: 22,
                       decoration: BoxDecoration(
                         color: Colors.yellow,
                         shape: BoxShape.circle,
                       ),
                     ),
-                    SizedBox(width: 10,),
+                    SizedBox(
+                      width: 10,
+                    ),
                     Container(
                       height: 22,
                       width: 22,
@@ -127,10 +159,40 @@ class DetailedScreen extends StatelessWidget {
               ],
             ),
           )
-
         ],
       ),
-
+      bottomSheet: BottomAppBar(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height / 10,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              )),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '\$' '${product.price}',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              ElevatedButton.icon(
+                style: ButtonStyle(),
+                onPressed: () {},
+                icon: Icon(Icons.send),
+                label: Text("Add to Cart"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
